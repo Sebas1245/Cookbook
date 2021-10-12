c2dcbfe7f424201600b22e7f5d137942dc9abf6c
Sebastián Saldaña <sebastian.saldana@wuhutech.com>
2021-10-12 14:05:12 -0500

    Adds connection to mongo db

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
+require('dotenv').config()
 const   express = require('express'), 
         app = express(),
+        dbConfig = require('./dbConfig');
         PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(require('cors')());
+
+// initialize db
+dbConfig();
 
 app.get('/', (req, res) => {
     res.json({msg: 'Hello from Cookbook index route!'});
---
+++ server/dbConfig.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+function setup() {
+    let uri = 'MONGODB_URI';
+    if (process.env.NODE_ENV === 'test') uri += '_TEST'
+    else if (process.env.NODE_ENV === 'dev') uri += '_DEV'
+    else if (process.env.NODE_ENV === 'prod') uri += '_PROD';
+    mongoose.connect(process.env[uri])
+        .then(result => console.log('Connection successful'))
+        .catch(err => console.log('Error in connection to db :', err))
+
+    mongoose.connection.on('error', error => console.log(`Connection to database failed: ${error}`));
+    mongoose.connection.on('connected', () => console.log(`Connected to database`));
+    mongoose.connection.on('disconnected', () => console.log(`Disconnected from database`));
+}
+
+module.exports = setup;
