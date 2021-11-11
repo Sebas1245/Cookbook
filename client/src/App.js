077e4677d887aab70152de4fcb515ce6247a2380
Sebastián Saldaña <sebastian.saldana@wuhutech.com>
2021-11-10 19:36:03 -0600

    Adds bootstrap theme with scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
     <div className="App">
       <p>Hello from Cookbook client!</p>
+      <div>
+        <Button variant="primary">Hello</Button>
+      </div>
     </div>
   );
 }
