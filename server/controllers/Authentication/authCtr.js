b31dc8cd57d90468aba85b5b9a2d39aa75df8f6b
Sebastián Saldaña <sebastian.saldana@wuhutech.com>
2021-11-25 18:25:20 -0600

    Modifies auth controllers to handle errors from the server

--- server/controllers/Authentication/authCtr.js
+++ server/controllers/Authentication/authCtr.js
@@ -5,12 +5,14 @@ const   User = require('../../models/User'),
 
 ctr.register = () => async (req,res,next) => {
     const { username, password, confirmPassword } = req.body;
+    if (!username) return Promise.reject( new CustomError(400, "You need a username to sign up!"));
     if (password != confirmPassword) return Promise.reject(new CustomError(400, "Passwords do not match"));
     console.log(username, password, confirmPassword)
     let user = new User({username, password});
     await user.save();
     const token = await createToken(user);
     res.status(201).json({
+        success: true,
         message: 'User created successfully',
         user,
         token
